2284c93af5173c167ae63318444a38e82a628bed
smazepaa <smazepa@kse.ord.ua>
2022-12-27 20:42:27 +0200

    added class paddle and drawing it on screen

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import pgzrun
+import pygame
 
 WIDTH = 600
-HEIGHT = 800
+HEIGHT = 400
+surface = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+class Paddle:
+    def __init__(self):
+        self.height = 15
+        self.width = 120
+        self.x = int(WIDTH/2 - self.width/2)
+        # the rectangle x position is the top left point of the rectangle
+        self.y = int(HEIGHT - self.height * 2)  # so it isn't at the very bottom
+        self.rectangle = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def draw(self):
+        pygame.draw.rect(surface, 'cyan', self.rectangle)
+
+
+paddle = Paddle()
 
 
 def draw():
-    pass
+    paddle.draw()
 
 
 def update(dt):
