fbf408e759e99283c176a225e330055bcc894e52
smazepaa <smazepa@kse.ord.ua>
2022-12-29 17:16:28 +0200

    added paddle movement

--- main.py
+++ main.py
@@ -6,8 +6,24 @@ HEIGHT = 400
 surface = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+
 class Paddle:
     def __init__(self):
+        self.goal = Vector(0, 0)
         self.height = 15
         self.width = 120
         self.x = int(WIDTH/2 - self.width/2)
@@ -15,19 +31,38 @@ class Paddle:
         self.y = int(HEIGHT - self.height * 2)  # so it isn't at the very bottom
         self.rectangle = pygame.Rect(self.x, self.y, self.width, self.height)
 
+    def where_to(self, pos):
+        # getting the coordinates of the mouse
+        self.goal = Vector(pos[0], self.y)
+
     def draw(self):
         pygame.draw.rect(surface, 'cyan', self.rectangle)
+
+    def update(self, dt):
+        # moving the paddle
+        self.x = self.goal.x - self.width / 2  # so that the cursor is in the middle
+        # checking if paddle is not 'outside' the screen
+        if self.goal.x < self.width / 2:  # < 60
+            self.x = 0
+        if self.goal.x > WIDTH - self.width / 2:  # > 540
+            self.x = WIDTH - self.width  # = 480
+        self.rectangle = pygame.Rect(self.x, self.y, self.width, self.height)
 
 
 paddle = Paddle()
 
 
+def on_mouse_move(pos):
+    paddle.where_to(pos)
+
+
 def draw():
+    screen.clear()
     paddle.draw()
 
 
 def update(dt):
-    pass
+    paddle.update(dt)
 
 
 pgzrun.go()
